47879789c870a6564ac2c08f880178c167cd5a6b
Alexander Hjelm <nick.nertova@gmail.com>
2019-01-16 21:19:31 +0100

    Add scroll animation

---
+++ Animation/OneTimeScrollAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimeScrollAnimation : MonoBehaviour
+{
+	[SerializeField] private Vector3 dir;
+	[SerializeField] private float dist;
+	[SerializeField] private float speed;
+	private Vector3 startPos;
+
+	private void Awake()
+	{
+		startPos = transform.position;
+	}
+
+	private void Update () {
+		if ((startPos - transform.position).magnitude < dist) {
+			transform.position += dir.normalized * speed * Time.deltaTime;
+		}
+	}
+}
