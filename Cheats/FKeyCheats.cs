512c342004ab5d50fd4edca09dcb9d3ef6cf632b
Alexander Hjelm <nick.nertova@gmail.com>
2019-03-14 15:21:13 +0100

    New cheats script

---
+++ Cheats/FKeyCheats.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class FKeyCheats : MonoBehaviour {
+	
+	void Update () {
+		if(!Application.isEditor) return;
+		
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F2))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F3))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F4))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F5))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F6))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F7))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F8))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F9))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F10))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F11))
+		{
+			
+		}
+		if (Input.GetKeyDown(KeyCode.F12))
+		{
+			
+		}
+	}
+}
